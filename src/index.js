5c2ae071c1657a38d3ed9f60530c1e939e6d4374
akmojahid <texthouse7@gmail.com>
2023-01-29 16:35:08 +0600

    project setuped

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 require("dotenv").config();
-const fs = require("fs");
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 4000;
+app.get("/", (req, res) => {});
 
-app.get("/", (req, res) => {
-  fs.rename("./test.ts", "test.js");
-
-  res.end();
-});
-
-app.listen(port, console.log(`Server in https://localhost:${port}`));
-
+const PORT = process.env.PORT || 401;
+app.listen(PORT, console.log(`https://localhost:${PORT}`));
