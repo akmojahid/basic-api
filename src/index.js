c4efd5f77240505ec4ca58eae1b9a3e766f2d9cf
akmojahid <texthouse7@gmail.com>
2023-01-31 18:13:39 +0600

    MongoDb practicce

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-app.get("/", (req, res) => {});
 
-const PORT = process.env.PORT || 401;
-app.listen(PORT, console.log(`https://localhost:${PORT}`));
+const uri = process.env.DB_URL;
+
+//routes
+app.post("/user", async (req, res) => {
+  mongoose
+    .connect(uri)
+    .then(() => console.log("connected"))
+    .catch((err) => console.log(err));
+  res.end();
+});
+
+app.get("/", (req, res) => {
+  res.json({ msg: "HEllO DEVELOPER" });
+});
+
+//listen\
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`https:localhost:${PORT}`));
